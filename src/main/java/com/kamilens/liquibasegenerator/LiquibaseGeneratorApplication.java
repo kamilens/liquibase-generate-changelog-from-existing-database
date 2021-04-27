db7788d6037314d3a88495f4483419c916c9ffe8
kamil.mehmandarov <kamil.mehmandarov@cybernet.az>
2021-04-27 10:10:05 +0400

    Initial commit

---
+++ src/main/java/com/kamilens/liquibasegenerator/LiquibaseGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.kamilens.liquibasegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseGeneratorApplication.class, args);
+	}
+
+}
